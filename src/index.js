d50c04dd615708bbeddab2d6b67e266d19cd9755
Chris Dukakis <chrisdukakis@gmail.com>
2022-04-24 11:07:55 +0300

    feat: use treesitter

--- src/index.js
+++ src/index.js
@@ -1,35 +1,15 @@
 'use strict';
 
-import lexer from 'node-c-lexer';
+import Parser from 'web-tree-sitter';
 
-const extractInterface = function (qubicName, sourceText) {
-  const tokens = lexer.lexUnit.tokenize(sourceText);
-  const extractedInterface = {
-    structures: [],
-    functions: [],
-  };
-  let structure = undefined;
-  let member = undefined;
-  let memberType = undefined;
-  let lastMember = undefined;
-  let comment = undefined;
+const extractInterface = async function (qubicName, sourceText) {
+  await Parser.init();
+  const parser = new Parser();
+  const Lang = await Parser.Language.load('tree-sitter-cpp.wasm');
+  parser.setLanguage(Lang);
+  const tree = parser.parse(sourceText);
 
-  const extractType = function(token) {
-    if (structure !== undefined && member === undefined) {
-      member = {
-        row: token.row,
-        col: token.col,
-      };
-      if (comment !== undefined) {
-        member.comment = comment;
-        comment = undefined;
-      }
-    }
-    
-    memberType += memberType === '' ? token.lexeme : (token.lexeme === '*' ? '' : ' ') + token.lexeme;
-  }
-
-  const stripCommentIdentifiers = function (comment) {
+  const trimComment = function (comment) {
     if (comment.slice(0, 2) === '//') {
       comment = comment.slice(2);
     } else {
@@ -38,134 +18,170 @@ const extractInterface = function (qubicName, sourceText) {
     return comment.trim();
   }
 
-  tokens.forEach(function (token, i) {
-    switch (token.tokenClass) {
-      case 'STRUCT':
-        structure = {
-          identifier: '',
-          members: [],
-          row: token.row,
-          col: token.col,
-        }
-        memberType = '';
-        break;
-
-      case 'COMMENT':
-        if (comment !== undefined) {
-          comment += "\n";
-          comment += stripCommentIdentifiers(token.lexeme);
-        } else {
-          comment = stripCommentIdentifiers(token.lexeme);
-        }
-
-        if (token.row === lastMember.row) {
-          if (lastMember.comment !== undefined) {
-            lastMember.comment += "\n";
-            lastMember.comment += comment;
-          } else {
-            lastMember.comment = comment;
+  const structs = [];
+  const functions = [];
+  let comment;
+  tree.rootNode.children.forEach(function (node) {
+    switch (node.type) {
+      case 'struct_specifier':
+        const struct = {};
+        node.children.forEach(function (node2) {
+          switch (node2.type) {
+            case 'type_identifier':
+              struct.type = {
+                text: node2.text,
+                startIndex: node2.startIndex,
+                endIndex: node2.endIndex,
+                startPosition: node2.startPosition,
+                endPosition: node2.endPosition,
+              }
+              break;
+            
+            case 'field_declaration_list':
+              struct.fields = [];
+              node2.children.forEach(function (node3) {
+                switch (node3.type) {
+                  case 'field_declaration':
+                    const field = {};
+                    node3.children.forEach(function (node4) {
+                      switch (node4.type) {
+                        case 'sized_type_specifier':
+                          field.type = {
+                            text: node4.text,
+                            startIndex: node4.startIndex,
+                            endIndex: node4.endIndex,
+                            startPosition: node4.startPosition,
+                            endPosition: node4.endPosition,
+                          };
+                          break;
+                        
+                        case 'primitive_type':
+                          field.type = {
+                            text: node4.text,
+                            startIndex: node4.startIndex,
+                            endIndex: node4.endIndex,
+                            startPosition: node4.startPosition,
+                            endPosition: node4.endPosition,
+                          };
+                          break;
+                        
+                        case 'pointer_declarator':
+                          field.type.text += '*';
+                          field.type.endIndex += node4.children[0].endIndex - node4.children[0].startIndex;
+                          field.type.endPosition = node4.children[0].endPosition;
+                          field.identifier = {
+                            text: node4.children[1].text,
+                            startIndex: node4.children[1].startIndex,
+                            endIndex: node4.children[1].endIndex,
+                            startPosition: node4.children[1].startPosition,
+                            endPosition: node4.children[1].endPosition,
+                          }
+                          if (comment !== undefined) {
+                            field.comment = comment;
+                            comment = undefined;
+                          }
+                          break;
+                      
+                        case 'field_identifier':
+                          field.identifier = {
+                            text: node4.text,
+                            startIndex: node4.startIndex,
+                            endIndex: node4.endIndex,
+                            startPosition: node4.startPosition,
+                            endPosition: node4.endPosition,
+                          }
+                          if (comment !== undefined) {
+                            field.comment = comment;
+                            comment = undefined;
+                          }
+                      }
+                    });
+                    struct.fields.push(field)
+                    break;
+                  
+                  case 'comment':
+                    comment = comment !== undefined ? comment + "\n" + trimComment(node3.text) : trimComment(node3.text);
+                    if (struct.fields.length > 0) {
+                      if (node3.startPosition.row === struct.fields[struct.fields.length - 1].identifier.startPosition.row) {
+                        struct.fields[struct.fields.length - 1].comment = 
+                          struct.fields[struct.fields.length - 1].comment !== undefined 
+                            ? struct.fields[struct.fields.length - 1].comment + "\n" + comment
+                            : comment;
+                        comment = undefined;
+                      }
+                    }
+                }
+              });
+            }
+          });
+        
+          if (struct.type.text.split('_')[0] === qubicName) {
+            structs.push(struct);
           }
-          comment = undefined;
-        }
-        break;
+          break;
+          
+          case 'comment':
+            comment = comment !== undefined ? comment + "\n" + trimComment(node.text) : trimComment(node.text);
+            break;
+          
+          case 'declaration':
+            const fn = {};
 
-      case 'IDENTIFIER':
-        if (structure?.identifier === '') {
-          structure.identifier = token.lexeme;
-        }
-
-        if (member !== undefined && memberType !== undefined) {
-          member.identifier = token.lexeme;
-          member.type = memberType;
-          memberType = undefined;
-        }
-        break;
-
-      case 'SIGNED':
-        extractType(token);
-        break;
-
-      case 'UNSIGNED':
-        extractType(token);
-        break;
-  
-      case 'BOOL':
-        extractType(token);
-        break;
-  
-      case 'CHAR':
-        extractType(token);
-        break;
-    
-      case 'SHORT':
-        extractType(token);
-        break;
-  
-      case 'INT':
-        extractType(token);
-        break;
-  
-      case 'LONG':
-        extractType(token);
-        break;
-  
-      case '*':
-        extractType(token);
-        break;
-
-      case 'VOID':
-        if (tokens[i + 1]?.lexeme?.split('_')?.[0] === qubicName) {
-          let j = i + 3;
-          if (tokens[j]) {
-            const arg = [];
-            while (tokens[j].tokenClass !== ')') {
-              arg.push(tokens[j])
-              j++;
-            }
-            const fn = {
-              row: token.row,
-              col: token.col,
-              identifier: tokens[i + 1].lexeme,
-              argument: {
-                row: arg[0].row,
-                col: arg[0].col,
-                type: arg.slice(0, -1).map(function (token) {
-                  return token.lexeme;
-                }).join(' '),
-                identifier: arg[arg.length - 1].lexeme,
-              },
-            }
             if (comment !== undefined) {
               fn.comment = comment;
               comment = undefined;
             }
-            lastMember = fn;
-            extractedInterface.functions.push(fn);
-          }
-        }
-        break;
 
-      case '}':
-        if (structure !== undefined) {
-          if (structure.identifier.split('_')[0] === qubicName) {
-            extractedInterface.structures.push(structure);
-          }
-          memberType = undefined;
-          structure = undefined;
-        }
-        break;
+            if (node.children[0]?.type === 'primitive_type' && node.children[0]?.text === 'void') {
+              fn.type = {
+                text: node.children[0].text,
+                startIndex: node.children[0].startIndex,
+                endIndex: node.children[0].endIndex,
+                startPosition: node.children[0].startPosition,
+                endPosition: node.children[0].endPosition,
+              }
+            }
 
-      case ';':
-        if (structure !== undefined && member !== undefined) {
-          structure.members.push(member);
-          lastMember = member;
-          member = undefined;
-        }
-        memberType = '';
+            fn.identifier = {
+              text: node.children[1].children[0].children[0].text,
+              startIndex: node.children[1].children[0].children[0].startIndex,
+              endIndex: node.children[1].children[0].children[0].endIndex,
+              startPosition: node.children[1].children[0].children[0].startPosition,
+              endPosition: node.children[1].children[0].children[0].endPosition,
+            }
+
+            fn.arguments = [];
+            node.children[1].children[0].children[1].children.forEach(function (node2) {
+              if (node2.type === 'parameter_declaration') {
+                fn.arguments.push({
+                  type: {
+                    text: node2.children[0].text,
+                    startIndex: node2.children[0].startIndex,
+                    endIndex: node2.children[0].endIndex,
+                    startPosition: node2.children[0].startPosition,
+                    endPosition: node2.children[0].endPosition,
+                  },
+                  identifier: {
+                    text: node2.children[1].text,
+                    startIndex: node2.children[1].startIndex,
+                    endIndex: node2.children[1].endIndex,
+                    startPosition: node2.children[1].startPosition,
+                    endPosition: node2.children[1].endPosition,
+                  }
+                })
+              }
+            });
+
+            if (fn.identifier !== undefined && fn.identifier.text.split('_')[0] === qubicName) {
+              functions.push(fn);
+            }
     }
   });
 
-  return extractedInterface;
+  return {
+    structs,
+    functions,
+  };
 }
 
 export default extractInterface;
--- test/extract.test.js
+++ test/extract.test.js
@@ -18,17 +18,28 @@ describe('extractInterface', function () {
         Additional description.
       */
       unsigned int testUInt; // Additional description.
+      /*
+        Test ptr.
+      */
       char* testCharPtr;
+      char* testCharPtr2;
     };
 
-    struct Test() {
+    struct SecureQus_Withdraw2 {
+      // Test.
+    };
+
+    struct Test {
       long long test;
     };
 
     /*
       Test.
     */
-    void SecureQus(Void effect) { // Test.
+    /*
+      Test2.
+    */
+    void SecureQus(Void effect) {
 
     };
 
@@ -36,97 +47,404 @@ describe('extractInterface', function () {
 
     };
 
-    void Test2() {
+    int Test2() {
+
+    };
+
+    void Test3() {
     };
 
     void SecureQus(
 `;
 
-  const actual = extractInterface('SecureQus', sourceText)
+  const awaitActual = extractInterface('SecureQus', sourceText)
 
   assert({
     given: 'source text',
     should: 'return correct interface',
-    actual,
-    expected: {
-      structures: [
+    awaitActual,
+    expected:    {
+      "structs": [
         {
-          col: 5,
-          identifier: "SecureQus_Withdraw",
-          members: [
+          "type": {
+            "text": "SecureQus_Withdraw",
+            "startIndex": 12,
+            "endIndex": 30,
+            "startPosition": {
+              "row": 1,
+              "column": 11
+            },
+            "endPosition": {
+              "row": 1,
+              "column": 29
+            }
+          },
+          "fields": [
             {
-              col: 7,
-              comment: "Amount description.",
-              identifier: "amount",
-              row: 3,
-              type: "long long",
+              "type": {
+                "text": "long long",
+                "startIndex": 39,
+                "endIndex": 48,
+                "startPosition": {
+                  "row": 2,
+                  "column": 6
+                },
+                "endPosition": {
+                  "row": 2,
+                  "column": 15
+                }
+              },
+              "identifier": {
+                "text": "amount",
+                "startIndex": 49,
+                "endIndex": 55,
+                "startPosition": {
+                  "row": 2,
+                  "column": 16
+                },
+                "endPosition": {
+                  "row": 2,
+                  "column": 22
+                }
+              },
+              "comment": "Amount description."
             },
             {
-              col: 7,
-              identifier: "testBool",
-              row: 4,
-              type: "bool",
+              "type": {
+                "text": "bool",
+                "startIndex": 86,
+                "endIndex": 90,
+                "startPosition": {
+                  "row": 3,
+                  "column": 6
+                },
+                "endPosition": {
+                  "row": 3,
+                  "column": 10
+                }
+              },
+              "identifier": {
+                "text": "testBool",
+                "startIndex": 91,
+                "endIndex": 99,
+                "startPosition": {
+                  "row": 3,
+                  "column": 11
+                },
+                "endPosition": {
+                  "row": 3,
+                  "column": 19
+                }
+              }
             },
             {
-              col: 7,
-              identifier: "testChar",
-              row: 5,
-              type: "char",
+              "type": {
+                "text": "char",
+                "startIndex": 107,
+                "endIndex": 111,
+                "startPosition": {
+                  "row": 4,
+                  "column": 6
+                },
+                "endPosition": {
+                  "row": 4,
+                  "column": 10
+                }
+              },
+              "identifier": {
+                "text": "testChar",
+                "startIndex": 112,
+                "endIndex": 120,
+                "startPosition": {
+                  "row": 4,
+                  "column": 11
+                },
+                "endPosition": {
+                  "row": 4,
+                  "column": 19
+                }
+              }
             },
             {
-              col: 7,
-              identifier: "testShort",
-              row: 6,
-              type: "short",
+              "type": {
+                "text": "short",
+                "startIndex": 128,
+                "endIndex": 133,
+                "startPosition": {
+                  "row": 5,
+                  "column": 6
+                },
+                "endPosition": {
+                  "row": 5,
+                  "column": 11
+                }
+              },
+              "identifier": {
+                "text": "testShort",
+                "startIndex": 134,
+                "endIndex": 143,
+                "startPosition": {
+                  "row": 5,
+                  "column": 12
+                },
+                "endPosition": {
+                  "row": 5,
+                  "column": 21
+                }
+              }
             },
             {
-              col: 7,
-              identifier: "testInt",
-              row: 7,
-              type: "signed int",
+              "type": {
+                "text": "signed int",
+                "startIndex": 151,
+                "endIndex": 161,
+                "startPosition": {
+                  "row": 6,
+                  "column": 6
+                },
+                "endPosition": {
+                  "row": 6,
+                  "column": 16
+                }
+              },
+              "identifier": {
+                "text": "testInt",
+                "startIndex": 162,
+                "endIndex": 169,
+                "startPosition": {
+                  "row": 6,
+                  "column": 17
+                },
+                "endPosition": {
+                  "row": 6,
+                  "column": 24
+                }
+              }
             },
             {
-              col: 7,
-              comment: "TestUint description.\nAdditional description.\nAdditional description.",
-              identifier: "testUInt",
-              row: 14,
-              type: "unsigned int",
+              "type": {
+                "text": "unsigned int",
+                "startIndex": 275,
+                "endIndex": 287,
+                "startPosition": {
+                  "row": 13,
+                  "column": 6
+                },
+                "endPosition": {
+                  "row": 13,
+                  "column": 18
+                }
+              },
+              "identifier": {
+                "text": "testUInt",
+                "startIndex": 288,
+                "endIndex": 296,
+                "startPosition": {
+                  "row": 13,
+                  "column": 19
+                },
+                "endPosition": {
+                  "row": 13,
+                  "column": 27
+                }
+              },
+              "comment": "TestUint description.\nAdditional description.\nAdditional description."
             },
             {
-              col: 7,
-              identifier: "testCharPtr",
-              row: 15,
-              type: "char*",
+              "type": {
+                "text": "char*",
+                "startIndex": 367,
+                "endIndex": 372,
+                "startPosition": {
+                  "row": 17,
+                  "column": 6
+                },
+                "endPosition": {
+                  "row": 17,
+                  "column": 11
+                }
+              },
+              "identifier": {
+                "text": "testCharPtr",
+                "startIndex": 373,
+                "endIndex": 384,
+                "startPosition": {
+                  "row": 17,
+                  "column": 12
+                },
+                "endPosition": {
+                  "row": 17,
+                  "column": 23
+                }
+              },
+              "comment": "Test ptr."
             },
-          ],
-          row: 2,
+            {
+              "type": {
+                "text": "char*",
+                "startIndex": 392,
+                "endIndex": 397,
+                "startPosition": {
+                  "row": 18,
+                  "column": 6
+                },
+                "endPosition": {
+                  "row": 18,
+                  "column": 11
+                }
+              },
+              "identifier": {
+                "text": "testCharPtr2",
+                "startIndex": 398,
+                "endIndex": 410,
+                "startPosition": {
+                  "row": 18,
+                  "column": 12
+                },
+                "endPosition": {
+                  "row": 18,
+                  "column": 24
+                }
+              }
+            }
+          ]
         },
+        {
+          "type": {
+            "text": "SecureQus_Withdraw2",
+            "startIndex": 431,
+            "endIndex": 450,
+            "startPosition": {
+              "row": 21,
+              "column": 11
+            },
+            "endPosition": {
+              "row": 21,
+              "column": 30
+            }
+          },
+          "fields": []
+        }
       ],
-      functions: [
+      "functions": [
         {
-          argument: {
-            col: 20,
-            identifier: "effect",
-            row: 25,
-            type: "Void"
+          "comment": "Test.\nTest2.",
+          "type": {
+            "text": "void",
+            "startIndex": 581,
+            "endIndex": 585,
+            "startPosition": {
+              "row": 35,
+              "column": 4
+            },
+            "endPosition": {
+              "row": 35,
+              "column": 8
+            }
           },
-          col: 5,
-          identifier: "SecureQus",
-          row: 25,
-          comment: "Test.\nTest.",
+          "identifier": {
+            "text": "SecureQus",
+            "startIndex": 586,
+            "endIndex": 595,
+            "startPosition": {
+              "row": 35,
+              "column": 9
+            },
+            "endPosition": {
+              "row": 35,
+              "column": 18
+            }
+          },
+          "arguments": [
+            {
+              "type": {
+                "text": "Void",
+                "startIndex": 596,
+                "endIndex": 600,
+                "startPosition": {
+                  "row": 35,
+                  "column": 19
+                },
+                "endPosition": {
+                  "row": 35,
+                  "column": 23
+                }
+              },
+              "identifier": {
+                "text": "effect",
+                "startIndex": 601,
+                "endIndex": 607,
+                "startPosition": {
+                  "row": 35,
+                  "column": 24
+                },
+                "endPosition": {
+                  "row": 35,
+                  "column": 30
+                }
+              }
+            }
+          ]
         },
         {
-          argument: {
-            col: 20,
-            identifier: "effect",
-            row: 29,
-            type: "SecureQus_Withdraw",
+          "type": {
+            "text": "void",
+            "startIndex": 624,
+            "endIndex": 628,
+            "startPosition": {
+              "row": 39,
+              "column": 4
+            },
+            "endPosition": {
+              "row": 39,
+              "column": 8
+            }
           },
-          col: 5,
-          identifier: "SecureQus",
-          row: 29,
+          "identifier": {
+            "text": "SecureQus",
+            "startIndex": 629,
+            "endIndex": 638,
+            "startPosition": {
+              "row": 39,
+              "column": 9
+            },
+            "endPosition": {
+              "row": 39,
+              "column": 18
+            }
+          },
+          "arguments": [
+            {
+              "type": {
+                "text": "SecureQus_Withdraw",
+                "startIndex": 639,
+                "endIndex": 657,
+                "startPosition": {
+                  "row": 39,
+                  "column": 19
+                },
+                "endPosition": {
+                  "row": 39,
+                  "column": 37
+                }
+              },
+              "identifier": {
+                "text": "effect",
+                "startIndex": 658,
+                "endIndex": 664,
+                "startPosition": {
+                  "row": 39,
+                  "column": 38
+                },
+                "endPosition": {
+                  "row": 39,
+                  "column": 44
+                }
+              }
+            }
+          ]
         }
       ]
-    },
+    }
   });
 });
